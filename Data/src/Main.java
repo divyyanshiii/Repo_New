68681300ce376023d041cbfd36becd03a20a3081
divyyanshiii <divyanshiii24082005@gmail.com>
2025-07-23 02:08:36 -0700

    Add files via upload

---
+++ Data/src/Main.java
@@ -0,0 +1,33 @@
+class SimpleCalculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero!");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+
+        System.out.println("x = " + x + ", y = " + y);
+        System.out.println("Addition: " + add(x, y));
+        System.out.println("Subtraction: " + subtract(x, y));
+        System.out.println("Multiplication: " + multiply(x, y));
+        System.out.println("Division: " + divide(x, y));
+    }
+}
